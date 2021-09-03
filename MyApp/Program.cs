e94f3ca538082df69bf0615681a0974838ef25df
Adrian Borup <avborup@gmail.com>
2021-09-03 14:10:47 +0200

    Tested and implemented IsLeapYear

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,11 +2,16 @@
 
 namespace MyApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static bool IsLeapYear(int year)
+        {
+            return (year % 100 != 0) && (year % 4 == 0) || (year % 400 == 0);
         }
     }
 }
